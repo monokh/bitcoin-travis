6fdd46887ed5573f8c7bf7b370ce8542dcb302e5
monokh <mnokhb@gmail.com>
2020-05-28 16:30:09 +0100

    The fix was the bitcoin version

--- mine.sh
+++ mine.sh
@@ -1,7 +1,5 @@
 apk add curl curl-dev
 
-sleep 10
-
-mine () { curl --basic -u bitcoin:local321 -X POST -d "{\"jsonrpc\": \"2.0\", \"id\": \"0\", \"method\": \"generate\", \"params\": [$1]}" http://${TARGET_HOST}:${TARGET_PORT}; }
+mine () { curl --basic -u bitcoin:local321 -X POST -d "{\"jsonrpc\": \"2.0\", \"id\": \"0\", \"method\": \"generate\", \"params\": [$1]}" http://${TARGET_HOST}:${TARGET_PORT} > /dev/null; }
 
 mine 105 # Extra few blocks for some mature funds
