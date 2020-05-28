b66d5976d1ebc086eda3081a6228a668df0edf04
monokh <mnokhb@gmail.com>
2020-05-28 14:57:48 +0100

    Log mining blocks

--- mine.sh
+++ mine.sh
@@ -1,5 +1,5 @@
 apk add curl curl-dev
 
-mine () { curl --basic -u bitcoin:local321 -X POST -d "{\"jsonrpc\": \"2.0\", \"id\": \"0\", \"method\": \"generate\", \"params\": [$1]}" http://${TARGET_HOST}:${TARGET_PORT} > /dev/null; }
+mine () { curl --basic -u bitcoin:local321 -X POST -d "{\"jsonrpc\": \"2.0\", \"id\": \"0\", \"method\": \"generate\", \"params\": [$1]}" http://${TARGET_HOST}:${TARGET_PORT}; }
 
 mine 105 # Extra few blocks for some mature funds
